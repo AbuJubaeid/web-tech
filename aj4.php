26561ef7224918eec8e7fe86131649b8071357cf
AbuJubaeid <106513861+AbuJubaeid@users.noreply.github.com>
2022-06-13 13:36:05 +0600

    Add files via upload
    Lab-04

--- aj4.php
+++ aj4.php
@@ -1,9 +1,74 @@
-<?php  
-$i;
-for($i=10; $i<100; $i++){  
-if($i%2!=0)  
-{  
- echo $i."<br>";   
-}    
-}  
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Registration</title>
+  </head>
+  <body>
+    <form>
+      <table border="1px" style="border-collapse: collapse">
+        <tr>
+          <td colspan="3">
+            <h2 align="center"><b>Registration</b></h2>
+          </td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td><input type="text" name="" value="" placeholder="username" /></td>
+          <td width="15px"></td>
+        </tr>
+
+        <tr>
+          <td>Gmail</td>
+          <td><input type="Gmail" name="" value="gmail id" /></td>
+          <td width="15px"></td>
+        </tr>
+        <tr>
+          <td>Gender</td>
+          <td>
+            <input type="radio" name="gender" value="" /> Male
+            <input type="radio" name="gender" value="" /> Female
+            <input type="radio" name="gender" value="" /> Other
+            <td width="15px"></td>
+          </td>
+        </tr>
+        <tr>
+          <td>Date of Birth</td>
+          <td><input type="date" name="" value="" /></td>
+          <td width="15px"></td>
+        </tr>
+
+        <tr>
+          <td>Blood Group</td>
+          <td>
+            <select name="">
+              <option value="">A+</option>
+              <option value="">A-</option>
+              <option value="">O+</option>
+              <option value="">O-</option>
+              <option value="">B+</option>
+              <option value="">B-</option>
+              <option value="">AB+</option>
+              <option value="">AB-</option>
+            </select>
+          </td>
+          <td width="15px"></td>
+        </tr>
+        <tr>
+          <td>Degree</td>
+          <td>
+            <input type="radio" name="degree" /> SSC
+            <input type="radio" name="degree" /> HSC
+            <input type="radio" name="degree" /> BSc.
+            <input type="radio" name="degree" /> MSc.
+          </td>
+        <tr>
+          <td align="right" colspan="3">
+            <input type="Save" name="" value="Save" />
+            <input type="submit" name="" value="Submit" />
+            <input type="reset" name="" value="Reset" />
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
